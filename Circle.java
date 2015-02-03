7340368e2da9deeeed82cf329bb81c52c979d6ae
Gavin <garlan0228@yahoo.com>
2015-02-02 21:49:00 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -4,7 +4,7 @@ import java.lang.*;
 class Circle
 {	
 	static Point[] points=new Point[2];
-	static double radius=new double[2];
+	static double[] radius=new double[2];
 	public static void main(String[] args)
 	{
 		Scanner input=new Scanner(System.in);
@@ -15,12 +15,12 @@ class Circle
 			System.out.printf("Please type in the y of the circle center %d \n",i+1);
 			double y=input.nextDouble();
 			System.out.printf("Please type in the radius of circle %d \n",i+1);
-			radius[i].nextDouble();
+			radius[i]=input.nextDouble();
 			Point point=new Point();
 			point.setLocation(x,y);
 			points[i]=point;
 		}
-		double centerDist=getDistance(point[0],point[1]);
+		double centerDist=getDistance(points[0],points[1]);
 		if(centerDist>radius[0]+radius[1])
 		{
 			System.out.println("They are separated from each other.");
@@ -32,7 +32,7 @@ class Circle
 			if(centerDist<radius[0]||centerDist<radius[1])
 				System.out.println("One of them is completely within another.");
 			else
-				System.out.println("They are overlapping.")
+				System.out.println("They are overlapping.");
 		}
 	}
 	public static double getDistance(Point p1,Point p2)
